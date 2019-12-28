362e714750bdf6ef846b3a99b702a71689f95fdf
wjp_jiaping@163.com <1099158648@qq.com>
2019-12-28 17:56:47 +0800

    策略模板详情

--- src/pages/securityCheck/policyTemplate/Page.vue
+++ src/pages/securityCheck/policyTemplate/Page.vue
@@ -9,29 +9,120 @@
           <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" style="margin-top: 20px;"></el-tree>
         </el-aside>
         <el-container>
-          <el-main>
-            <el-button icon="el-icon-plus">新增</el-button>
-            <el-button icon="el-icon-close">删除</el-button>
-            <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%;margin-top: 20px;"
-              @selection-change="handleSelectionChange">
-              <el-table-column type="selection" width="55"></el-table-column>
-              <el-table-column prop="name" label="策略模板名称"></el-table-column>
-              <el-table-column prop="type" label="检查项/设备类型"></el-table-column>
-              <el-table-column prop="attr" label="模板属性"></el-table-column>
-              <el-table-column prop="edit" label="编辑">
-                <template slot-scope="scope">{{'-'}}</template>
-              </el-table-column>
-              <el-table-column prop="custom" label="自定义参数">
-                <template slot-scope="scope">
-                  <i class="el-icon-s-tools" @click="checkList = true"></i>
-                </template>
-              </el-table-column>
-              <el-table-column fixed="right" label="详情" width="100">
-                <template slot-scope="scope">
-                  <el-button @click="handleClick(scope.row)" type="text" size="small">[详细]</el-button>
-                </template>
-              </el-table-column>
-            </el-table>
+          <el-main style="position: relative;">
+            <template v-if="!itemDetail">
+              <el-button icon="el-icon-plus">新增</el-button>
+              <el-button icon="el-icon-close">删除</el-button>
+              <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%;margin-top: 20px;">
+                <el-table-column type="selection" width="55"></el-table-column>
+                <el-table-column prop="name" label="策略模板名称"></el-table-column>
+                <el-table-column prop="type" label="检查项/设备类型"></el-table-column>
+                <el-table-column prop="attr" label="模板属性"></el-table-column>
+                <el-table-column prop="edit" label="编辑">
+                  <template slot-scope="scope">{{'-'}}</template>
+                </el-table-column>
+                <el-table-column prop="custom" label="自定义参数">
+                  <template slot-scope="scope">
+                    <i class="el-icon-s-tools" @click="checkList = true"></i>
+                  </template>
+                </el-table-column>
+                <el-table-column fixed="right" label="详情" width="100">
+                  <template slot-scope="scope">
+                    <el-button @click="handleClick(scope.row)" type="text" size="small">[详细]</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </template>
+            <template v-else>
+              <span class="el-icon-refresh-left re-back" @click="itemDetail = false">返回</span>
+              <el-tabs v-model="activeName" type="card">
+                <el-tab-pane label="Linux主机配置策略的检查项列表" name="first">
+                  <template>
+                    <el-table :data="checkItemData" border style="width: 100%">
+                      <el-table-column prop="check_num" label="检查项编号"></el-table-column>
+                      <el-table-column prop="check_name" label="检查项名称"></el-table-column>
+                      <el-table-column prop="check_type" label="类型"></el-table-column>
+                      <el-table-column fixed="right" label="操作" width="100">
+                        <template slot-scope="scope">
+                          <el-button @click="checkItemDetail = true" type="text" size="small">查看</el-button>
+                        </template>
+                      </el-table-column>
+                    </el-table>
+                  </template>
+                </el-tab-pane>
+              </el-tabs>
+              <el-dialog title="检查项详情" :visible.sync="checkItemDetail" width="60%">
+                <el-tabs v-model="checkItemDetailName" type="card">
+                  <el-tab-pane label="基本信息" name="basicInfo">
+                    <el-form label-position="left" label-width="120px" :model="checkBasicInfo">
+                      <el-form-item label="检查项名称">
+                        <el-input v-model="checkBasicInfo.name" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                      <el-form-item label="设备类型">
+                        <el-input v-model="checkBasicInfo.type" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                      <el-form-item label="检查项编号">
+                        <el-input v-model="checkBasicInfo.num" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                      <el-form-item label="所属分类">
+                        <el-select v-model="checkBasicInfo.itemize" placeholder="--请选择--" :disabled="checkBasicInfo.disable">
+                          <el-option label="账号管理" value="1"></el-option>
+                          <el-option label="账号管理" value="2"></el-option>
+                          <el-option label="账号管理" value="3"></el-option>
+                        </el-select>
+                      </el-form-item>
+                      <el-form-item label="自定义组">
+                        <el-input v-model="checkBasicInfo.custom" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                      <el-form-item label="配置要求">
+                        <el-input type="textarea" v-model="checkBasicInfo.allocation" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                      <el-form-item label="检测方法与依据">
+                        <el-input type="textarea" v-model="checkBasicInfo.basis" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                      <el-form-item label="加固方案">
+                        <el-input type="textarea" v-model="checkBasicInfo.programme" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                      <el-form-item label="权重">
+                        <el-select v-model="checkBasicInfo.weight" placeholder="--请选择--" :disabled="checkBasicInfo.disable">
+                          <el-option label="1" value="1"></el-option>
+                          <el-option label="2" value="2"></el-option>
+                          <el-option label="3" value="3"></el-option>
+                        </el-select>
+                      </el-form-item>
+                    </el-form>
+                  </el-tab-pane>
+                  <el-tab-pane label="核查信息" name="checkInfo">
+                    <el-form label-position="left" label-width="120px" :model="checkBasicInfo">
+                      <el-form-item label="检查方式">
+                        <el-radio-group v-model="checkBasicInfo.mode" :disabled="checkBasicInfo.disable">
+                          <el-radio label="自动" value="1"></el-radio>
+                          <el-radio label="手动" value="2"></el-radio>
+                        </el-radio-group>
+                      </el-form-item>
+                      <el-form-item label="检查脚本">
+                        <el-input v-model="checkBasicInfo.script" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                      <el-form-item label="解析方式">
+                        <el-radio-group v-model="checkBasicInfo.parsingWay" :disabled="checkBasicInfo.disable">
+                          <el-radio label="正则表达式" value="1"></el-radio>
+                          <el-radio label="JavaScript函数" value="2"></el-radio>
+                        </el-radio-group>
+                      </el-form-item>
+                      <el-form-item label="检查点">
+                        <el-input type="textarea" v-model="checkBasicInfo.point" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                      <el-form-item label="检查点规则">
+                        <el-input type="textarea" v-model="checkBasicInfo.rule" :disabled="checkBasicInfo.disable"></el-input>
+                      </el-form-item>
+                    </el-form>
+                  </el-tab-pane>
+                </el-tabs>
+                <div slot="footer" class="dialog-footer">
+                  <el-button @click="checkItemDetail = false">返 回</el-button>
+                </div>
+              </el-dialog>
+            </template>
           </el-main>
         </el-container>
       </el-container>
@@ -75,6 +166,7 @@
         attr: '预定义'
       };
       return {
+        activeName: 'first',
         tableData: Array(6).fill(item),
         data: [{
           label: '内置策略组',
@@ -111,6 +203,7 @@
         },
         editTree: false, // 添加或者编辑树形结构dialog
         checkList: false, // 核查项列表dialog
+        checkItemDetail: false, // 检查项详情dialog
         form: {
           name: '',
           desc: ''
@@ -124,7 +217,37 @@
             check_desc: '设置口令认证失败后锁定策略',
             check_val: '5'
           }
-        ]
+        ],
+        itemDetail: false, // 详情
+        checkItemData: [
+          {
+            check_num: 'Linux-1',
+            check_name: '口令锁定策略',
+            check_type: '/主机/Linux'
+          }, {
+            check_num: 'Linux-2',
+            check_name: '查找未授权的SUID-SGID文件',
+            check_type: '/主机/Linux'
+          }
+        ],
+        checkItemDetailName: 'basicInfo',
+        checkBasicInfo: {
+          name: '口令锁定策略', // 检查项名称
+          type: '/主机/Linux', // 设备类型
+          num: 'Linux-1', // 检查项编号
+          itemize: '1', // 所属分类
+          custom: '/自定义分组/自定义1', // 自定义组
+          allocation: '对于采用静态口令认证技术的设备，应配置当用户连续认证失败次数超过6次（不含6次），锁定该用户使用的账号', // 配置要求
+          basis: '判定依据', // 检测方法与判断依据
+          programme: '参考配置操作： 1.执行备份 2.修改策略设', // 加固方案
+          weight: '2',  // 权重
+          mode: '1', // 检查方式
+          script: '检查口令锁定策略', // 检查脚本
+          parsingWay: '1', // 解析方式
+          point: '', // 检查点
+          rule: 'v0 && v1', // 检查点规则
+          disable: true
+        }
       }
     },
     methods: {
@@ -177,6 +300,9 @@
           message: _messages,
           type: 'info'
         });
+      },
+      handleClick (row) {
+        this.itemDetail = !this.itemDetail
       }
 
     }
@@ -184,6 +310,13 @@
 </script>
 <style lang="less">
 .goodsTop{
-
+  .re-back {
+    display: inline-block;
+    position: absolute;
+    cursor: pointer;
+    right: 20px;
+    top: 30px;
+    z-index: 99999;
+  }
 }
 </style>
